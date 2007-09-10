c974ff213f43409952a887b4a7977b7a41acf015
Jim Mulholland <jim@squeejee.com>
2007-09-10 14:26:13 +0000

    Updated so all players appear in NFL Players drop down by default.

--- app/helpers/auctions_helper.rb
+++ app/helpers/auctions_helper.rb
@@ -1,6 +1,6 @@
 module AuctionsHelper
   
-  def display_nfl_player_dropdown(position_id)
+  def display_nfl_player_dropdown(position_id = nil)
     @nfl_players = NflPlayer.find_by_position_id(position_id)
     detail_collection_select(:auction, :nfl_player_id, @nfl_players, :id, :display_name, "NFL Players", "Choose One", options ={:prompt => "--Select a player--"} ) 
   end
