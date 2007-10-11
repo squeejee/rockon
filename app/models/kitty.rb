951911cee3e4ee45fdd51e881a5f28339652118f
Jim Mulholland <jim@squeejee.com>
2007-10-11 03:28:51 +0000

    Added method to auto-add auction payments to kitty

--- app/models/kitty.rb
+++ app/models/kitty.rb
@@ -47,4 +47,22 @@ SQL_STRING
   def self.sum_league_paid(user_id = current_user)
     return Kitty.sum(:league_paid, :conditions => ["user_id = ?", user_id])
   end
+  
+  def self.add_auctions_to_kitty(week_no = League.current_week)
+    a = Auction.find_all_by_week_no week_no
+    k = Kitty.find_all_by_week_no week_no
+
+    #Update the kitty unless the kitty already has the auction updates for that week
+    unless k.length >= a.length
+      a.each do |auction| 
+        Kitty.create(
+        {
+          :week_no =>  auction.week_no, 
+          :user_id => auction.top_bidder.user_id, 
+          :description =>  auction.top_bidder.user.full_name + " dropped " + auction.top_bidder.nfl_player.display_name + "; Picked-up " + auction.nfl_player.display_name, 
+          :league_due => auction.top_bidder.price
+        })
+      end
+    end
+  end
 end
