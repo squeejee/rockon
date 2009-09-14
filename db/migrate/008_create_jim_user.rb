23cf56cc924c6e6a150ce2f9fb618f245f84c16b
Jim Mulholland <jim@jim-mb-pro-2.local>
2009-09-14 09:24:44 -0500

    Fixed password change issue

--- db/migrate/008_create_jim_user.rb
+++ db/migrate/008_create_jim_user.rb
@@ -1,11 +1,16 @@
 class CreateJimUser < ActiveRecord::Migration
   def self.up
-    u = User.new
-    u.login="jim"
-    u.commish=true
-    u.password="password"
-    u.password_confirmation="password"
-    u.save
+    [['jim', 'jim.mulholland@gmail.com', true], ['bill', 'bill@billdubrul.com'],['curtis', 'CJohnson@grothcorp.com'],
+    ['frank', 'feverlyklm@yahoo.com'],['dwayne', 'johnsonda1@hotmail.com'],['jay', 'jjohnson7@gmail.com'],
+    ['joe', 'jobu93ag@yahoo.com'],['len', 'psu1993@yahoo.com'],['matt', 'mafia93@yahoo.com'],['mike', 'mike.mulholland@gmail.com']].each do |login, email, commish|
+      u = User.new
+      u.login=login
+      u.email = email
+      u.commish=true if commish
+      u.password="password"
+      u.password_confirmation="password"
+      u.save
+    end
   end
 
   def self.down
